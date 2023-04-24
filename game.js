9ce1d9edbc05e51986cb2c8c0c5c887d5edd9520
achucarrob <achucarroclaudia@gmail.com>
2023-04-24 10:02:51 -0400

    Cambios en la logica responsive

--- game.js
+++ game.js
@@ -2,46 +2,40 @@ const canvas = document.getElementById("canvas"); // Hacemos referencia a el esp
 const ctx = canvas.getContext("2d");
 
 
-window.addEventListener('load', startGame);
+window.addEventListener('load', canvasResponsive);
 // escuchar el evento resize y ejecutar la funcion
-// window.addEventListener('resize', startGame)
+window.addEventListener('resize', canvasResponsive)
 //  let windowWidth = window.innerWidth ;
 //  let windowHeight = window.innerHeight ;
 
+let canvasSize;
+let elementsSize;
 
-function startGame(){
-    let canvasSize;
-    
-    if (window.innerHeight > window.innerWidth) {
-      canvasSize = window.innerWidth * 0.8;
-    } else {
-      canvasSize = window.innerHeight * 0.8 ;
-    };
-    
-    canvas.setAttribute('width', canvasSize);
-    canvas.setAttribute('height', canvasSize);
 
-    const elementsSize = canvasSize / 10;
-    console.log({canvasSize, elementsSize});
-    ctx.font = elementsSize + 'px Verdana';
-    ctx.textAlign = 'end';
+//Funcion que obtiene las dimensiones de la ventana
+function canvasResponsive(){
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8 ;
+  };
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  // canvas.setAttribute('width', windowWidth * 0.75);
+  // canvas.setAttribute('height', windowHeight * 0.80);
 
-    for (let i = 1; i <= 10; i++){
-      //ctx.fillText(emojis['X'], 1 , elementsSizeVertical * i);
-      ctx.fillText(emojis['X'], elementsSize * i, elementsSize * i);
-    };
-    
-    // if (canvas.getContext) {
-      // const ctx = canvas.getContext("2d"); // llenamos el espacio vacio en canvas con el tipo de contexto en este caso 2d
-        // drawing code here
-      //   ctx.fillRect(25, 25, 100, 100); // un rectangulo :)
-      // } else {
-        // canvas-unsupported code here
-      // };
+  startGame()
 };
 
-// Funcion que obtiene las dimensiones de la ventana
-// function canvasResponsive(){
-//   canvas.setAttribute('width', windowWidth * 0.75);
-//   canvas.setAttribute('height', windowHeight * 0.80);
-// };
+function startGame(){
+    elementsSize = canvasSize / 10;
+    console.log({canvasSize, elementsSize});
+    ctx.font = elementsSize + 'px AriL';
+    ctx.textAlign = 'end';
+
+    for (let i = 0; i <= 10; i++){
+      //ctx.fillText(emojis['X'], 1 , elementsSizeVertical * i);
+      ctx.fillText(emojis['X'], elementsSize * i, elementsSize);
+    };
+};
