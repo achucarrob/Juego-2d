6b37fe719ecd4ac75ef4da63b18a033b5621a74b
achucarrob <achucarroclaudia@gmail.com>
2023-05-10 16:40:51 -0400

    Render de niveles

--- game.js
+++ game.js
@@ -29,23 +29,34 @@ function canvasResponsive(){
 };
 
 function startGame(){
-    // split method to separate a given string by the paramet given between () into arrays
-    // metodo .trim limpia los espacios de un string ubicados al principio o al final
-    const map = maps[1] // traemos los mapas, vienen como string hay que convertirlos a arrays con .split
-    const firstsplit = map.trim().split('\n'); // crear un array multidimencional por cada salto de linea \n
-    const secondsplit = firstsplit.map(fila => fila.trim().split(''))
-    console.log(map,firstsplit)
+  
+  elementsSize = canvasSize / 10;
+  console.log({canvasSize, elementsSize});
+  ctx.font = elementsSize + 'px Verdana';
+  ctx.textAlign = 'end';
 
-    elementsSize = canvasSize / 10;
-    console.log({canvasSize, elementsSize});
-    ctx.font = elementsSize + 'px Verdana';
-    ctx.textAlign = 'end';
+  // split method to separate a given string by the paramet given between () into arrays
+  // metodo .trim limpia los espacios de un string ubicados al principio o al final
+  const map = maps[2] // traemos los mapas, vienen como string hay que convertirlos a arrays con .split
+  const firstsplit = map.trim().split('\n'); // crear un array por cada salto de linea \n
+  const secondsplit = firstsplit.map(fila => fila.trim().split('')) // crea un array de los array anteriores (array multidimensinal) y separa cada string como un elemento
+  console.log(firstsplit,secondsplit)
 
-    
 
-    // for (let row = 1; row <= 10; row++){
-    //   for (let col = 1; col <= 10; col++){
-    //   ctx.fillText(emojis[secondsplit[row - 1][col - 1]], elementsSize * row , elementsSize * col);
-    //   }
-    // };
+  // recorrer el arreglo secondsplit
+  // El método forEach() ejecuta la función indicada una vez por cada elemento del array.
+  secondsplit.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
+      ctx.fillText(emoji, posX, posY);
+    });
+  });
+
+  // for (let row = 1; row <= 10; row++){
+  //   for (let col = 1; col <= 10; col++){
+  //   ctx.fillText(emojis[secondsplit[row - 1][col - 1]], elementsSize * row , elementsSize * col);
+  //   }
+  // };
 };
